91fa47376072496ffefc92e0ecd8cd061d32accb
unknown <jr.jardimmatos@gmail.com>
2021-08-22 00:31:36 -0300

    Lombok adicionado

--- src/main/java/com/jrdeveloper/servicos/api/dto/AtualizaStatusDTO.java
+++ src/main/java/com/jrdeveloper/servicos/api/dto/AtualizaStatusDTO.java
@@ -1,4 +1,12 @@
 package com.jrdeveloper.servicos.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AtualizaStatusDTO {
+    private String status;
 }
