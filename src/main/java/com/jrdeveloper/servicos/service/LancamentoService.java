5f2e48ac26301b82e19f5812417c8e86cc79fdfa
unknown <jr.jardimmatos@gmail.com>
2021-08-22 00:36:15 -0300

    Obter por id e obter saldo

--- src/main/java/com/jrdeveloper/servicos/service/LancamentoService.java
+++ src/main/java/com/jrdeveloper/servicos/service/LancamentoService.java
@@ -3,7 +3,9 @@ package com.jrdeveloper.servicos.service;
 import com.jrdeveloper.servicos.model.entity.Lancamento;
 import com.jrdeveloper.servicos.model.enums.StatusLancamento;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface LancamentoService {
 
@@ -18,4 +20,8 @@ public interface LancamentoService {
     void atualizarStatus(Lancamento lancamento, StatusLancamento status);
 
     void validar(Lancamento lancamento);
+
+    Optional<Lancamento> obterPorId(Long id);
+
+    BigDecimal obterSaldoUsuario(Long id);
 }
