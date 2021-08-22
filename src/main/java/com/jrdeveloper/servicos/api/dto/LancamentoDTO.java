32f733746471aa3417e3702a6f1425fd1a3c1fdd
unknown <jr.jardimmatos@gmail.com>
2021-08-22 00:32:16 -0300

    dto para Lancamento

--- src/main/java/com/jrdeveloper/servicos/api/dto/LancamentoDTO.java
+++ src/main/java/com/jrdeveloper/servicos/api/dto/LancamentoDTO.java
@@ -1,2 +1,20 @@
-package com.jrdeveloper.servicos.api.dto;public class LancamentoDTO {
+package com.jrdeveloper.servicos.api.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Builder
+@Data
+public class LancamentoDTO {
+
+    private Long id;
+    private String descricao;
+    private Integer mes;
+    private Integer ano;
+    private BigDecimal valor;
+    private Long usuario;
+    private String tipo;
+    private String status;
 }
